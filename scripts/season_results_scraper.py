663127341bc79444521dc9f96f1fc3bc93c2b7e8
CALEB_FORNSHELL <caleb.j.fornshell@gmail.com>
2025-05-04 12:03:41 -0500

    made into function

--- scripts/season_results_scraper.py
+++ scripts/season_results_scraper.py
@@ -7,29 +7,30 @@ import re # regular expressions
 import pandas as pd
 from bs4 import BeautifulSoup
 
-#need to create a function
-url = "https://www.baseball-reference.com/leagues/majors/2024-standings.shtml"
+def get_season_results(season):
 
-response = requests.get(url)
-html = response.text
+    url = "https://www.baseball-reference.com/leagues/majors/" + str(season) + "-standings.shtml"
+    response = requests.get(url)
+    html = response.text
 
+    #Removing html comment that messes with BeutifulSoup
+    html = re.sub(r'<!--.*-->', '', html)
 
-#website html had a wierd comment that BeautifulSoup wouldn't parse following html. 
-#I removed all comments and then it worked
-html = re.sub(r'<!--.*-->', '', html)
-
-
-soup = BeautifulSoup(html, features='html.parser')
-season_results = soup.find_all('tbody')
+    soup = BeautifulSoup(html, features='html.parser')
+    season_results = soup.find_all('tbody')
 
 
-df = pd.DataFrame(columns=['Season', 'Team', 'Wins', 'Losses', 'Win%', 'GB'])
+    df = pd.DataFrame(columns=['Season', 'Team', 'Wins', 'Losses', 'Win%', 'GB'])
 
-for tbl in season_results:
-    for row in tbl.find_all('tr'):
-        stat_row = [2024]
-        for stat in row.children:
-            stat_row.append(stat.text)
-        df.loc[len(df)] = stat_row
-        
-print(df)
+    for tbl in season_results:
+        for row in tbl.find_all('tr'):
+            stat_row = [season]
+            for stat in row.children:
+                stat_row.append(stat.text)
+            df.loc[len(df)] = stat_row
+
+    return(df)
+
+
+#print(get_season_results(2022))
+
